ac7f9363e9bc75f1d21bfb24e32a01f015a96009
林炳兴 <linbingxing@zuipin.cn>
2019-11-02 16:56:45 +0800

    坦克开启之旅-启程

---
+++ tank-admin/src/main/java/com/tank/TankAdminApplication.java
@@ -0,0 +1,18 @@
+package com.tank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @description 启动类
+  * @author lbx
+  * @date 2019/11/2 16:41
+  * @version 1.0.0
+ **/
+@SpringBootApplication
+public class TankAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankAdminApplication.class,args);
+    }
+}
